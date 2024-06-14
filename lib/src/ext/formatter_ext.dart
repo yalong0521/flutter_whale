a6d14ad699c83b576f8bf57b91b1c6ac34301343
weiyalong <10295010@qq.com>
2024-06-14 17:57:46 +0800

    修改正则

--- lib/src/ext/formatter_ext.dart
+++ lib/src/ext/formatter_ext.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/services.dart';
 
-final _doubleExp = RegExp(r'^(?!0\d|0$|\d*\.\d{3,}|^\.)(\d+|\d*\.\d{0,2})?$');
+/// 任意2位小数类型，可为0
+final _doubleExp = RegExp(r'^(?!0\d|\d*\.\d{3,}|^\.)((0|[1-9]\d*)(\.\d{0,2})?)?$');
+
+/// 1位任意数字
 final _singleNumExp = RegExp(r'^([0-9])?$');
+
+/// 2位数字，首位不能为0
 final _doubleNumExp = RegExp(r'^(?!0)\d{0,2}$');
 
 var doubleTextInputFormatter = CustomInputFormatter.fromRegExp(_doubleExp);
